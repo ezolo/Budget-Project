6bf1fbb15142114cdf43aa9dccc9ae667494b2af
Eva Zolotarev <70411084+ezolo@users.noreply.github.com>
2025-04-09 13:57:35 -0500

    Add files via upload
    Made changes to DDL & DML files

---
+++ DML.sql
@@ -0,0 +1,6 @@
+-- DML
+-- users mock data
+INSERT INTO users (name, username, email, password) VALUES
+('Default User', 'defaultuser', 'default@example.com', 'defaultpass'),
+('John Smith', 'jSmith', 'john@example.com', 'password123'),
+('Jane Smith', 'jSmith1', 'jane@example.com', 'password321');
